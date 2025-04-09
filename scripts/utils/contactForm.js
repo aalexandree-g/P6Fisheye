96a7e1d213e7eda5d2210db2448beabe20a7cbf7
aalexandree-g <aalexandree.g@gmail.com>
2025-04-09 16:49:25 +0200

    Erreurs dans le fichier json corrigées

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -15,7 +15,7 @@ export default class ContactForm {
             element.classList.remove("showing")
             element.classList.add("hiding")
             setTimeout(() => {
-                document.querySelector(".modal h2").textContent = ""
+                document.querySelector(".modal h2").innerHTML = ""
                 element.classList.remove("visible", "hiding")
             }, 250)
         })
@@ -30,6 +30,16 @@ export default class ContactForm {
             element.addEventListener("click", () => {
                 this.closeModal()
             })
+        })
+
+        document.querySelector(".submit_button").addEventListener("click", (e) => {
+            e.preventDefault()
+            const firstnameInput = document.getElementById("firstname").value
+            const lastnameInput = document.getElementById("lastname").value
+            const emailInput = document.getElementById("email").value
+            const messageInput = document.getElementById("message").value
+
+            console.log(`Les données invalides ne sont pas vérifiées.\nPrénom : ${firstnameInput}\nNom : ${lastnameInput}\nEmail : ${emailInput}\nMessage : ${messageInput}`)
         })
     }
 }
